0e3ab570ebc2bdb6a686a07663dbac1dacd67673
Orion68 <orions68@gmail.com>
2025-05-23 02:16:13 +0100

    Update Nexus-Astralis

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Nexus-Astralis' title`, () => {
+  it(`should have the 'nexus-astralis-front' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Nexus-Astralis');
+    expect(app.title).toEqual('nexus-astralis-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Nexus-Astralis');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nexus-astralis-front');
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, withComponentInputBinding, InMemoryScrollingOptions } from '@angular/router';
+import { withInMemoryScrolling } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const scrollConfig: InMemoryScrollingOptions = {
+  anchorScrolling: 'enabled',
+  scrollPositionRestoration: 'enabled',
+};
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling(scrollConfig),
+      withComponentInputBinding()
+    ),
+    provideHttpClient(
+      withFetch()
+    )
+  ]
+}).catch(err => console.error(err));
